5698ac392266c4ac1e1b62a9c483f0f67772c5b2
Almusawee <112513161+Almusawee@users.noreply.github.com>
2022-09-06 00:23:55 +0430

    3 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import {HiOutlineHome} from "react-icons/hi"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="flex ">
-      
+      <div>
+        <HiOutlineHome  className='' />
+      </div>
+      <h1>Wecome t NorthBy</h1>
+      <h2>A premium in sight and sound</h2>
+      <button>Learn more</button>
     </div>
   )
 }
