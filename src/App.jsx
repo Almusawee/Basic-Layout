e0f4461d8c7b83af68dee3d6033da58bcad29312
Almusawee <112513161+Almusawee@users.noreply.github.com>
2022-09-06 20:44:22 +0430

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
       <div className='mx-4 order-last'>
         <HiOutlineHome  className=' stroke-cyan-300 hover:stroke-cyan-700 ' size={100,100} />
       </div>
-      <div className='mx-4 self-center text-center background-image:url(./assets/img.png)'>
-      <h1 className='text-5xl py-1 font-bold text-blue-700'>Welcome to Almsawee Layout</h1>
-      <h2 className='text-3xl py-2 font-semibold text-blue-300'>A premium website UI/UX designs</h2>
+      <div className='mx-4 self-center text-center bg-auto '>
+      <h1 className='text-5xl py-1 font-bold text-blue-700'>Welcome to Almusawee Layout</h1>
+      <h2 className='text-3xl py-2 font-semibold text-blue-300'>A premium websites' UI/UX designs center</h2>
       <button className='my-4 px-4 py-2 border-2 border-transparent rounded-md text-white bg-blue-900 hover:animate-pulse animate-bounce hover:text-slate-900 hover:bg-sky-700 '>Learn more</button>
       </div>
     </div>
