f0800cb18e6edb224938f83527c4a517ab7e3389
Almusawee <112513161+Almusawee@users.noreply.github.com>
2022-09-06 19:58:09 +0430

    1 file modified, 1 added, 1 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex justify-center bg-gray-200  ">
+    <div className="flex justify-center bg-gray-200">
       <div className='mx-4 order-last'>
         <HiOutlineHome  className=' stroke-cyan-300 hover:stroke-cyan-700 ' size={100,100} />
       </div>
-      <div className='mx-4 self-center text-center'>
+      <div className='mx-4 self-center text-center background-image:url(./src/styles/img.png)'>
       <h1 className='text-5xl py-1 font-bold text-blue-700'>Welcome to Almsawee Layout</h1>
       <h2 className='text-3xl py-2 font-semibold text-blue-300'>A premium website UI/UX designs</h2>
       <button className='my-4 px-4 py-2 border-2 border-transparent rounded-md text-white bg-blue-900 hover:animate-pulse animate-bounce hover:text-slate-900 hover:bg-sky-700 '>Learn more</button>
