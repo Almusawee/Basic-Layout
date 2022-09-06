fbabf6ba6e8f2d09684aa70523d263c8eef30410
Almusawee <112513161+Almusawee@users.noreply.github.com>
2022-09-06 11:43:35 +0430

    4 files modified, 1 added, 2 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import {HiOutlineHome} from "react-icons/hi"
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex justify-center bg-gray-300 ">
+    <div className="flex justify-center bg-gray-200  ">
       <div className='mx-4 order-last'>
-        <HiOutlineHome  className='rounded-full stroke-cyan-300 hover:stroke-cyan-700' size={100,100} />
+        <HiOutlineHome  className=' stroke-cyan-300 hover:stroke-cyan-700' size={100,100} />
       </div>
       <div className='mx-4 self-center text-center'>
-      <h1 className='text-6xl font-bold text-blue-700'>Welcome to Almsawee Layout</h1>
-      <h2 className='text-3xl font-semibold text-blue-300'>A premium website UI/UX designs</h2>
-      <button className='my-4 px-4 py-2 border-2 border-black rounded-lg text-white bg-blue-900'>Learn more</button>
+      <h1 className='text-5xl py-1 font-bold text-blue-700'>Welcome to Almsawee Layout</h1>
+      <h2 className='text-3xl py-2 font-semibold text-blue-300'>A premium website UI/UX designs</h2>
+      <button className='my-4 px-4 py-2 border-2 border-black rounded-lg text-white bg-blue-900 hover:animate-pulse animate-bounce hover:text-slate-900 hover:bg-sky-700 '>Learn more</button>
       </div>
     </div>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,jsx}"],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
